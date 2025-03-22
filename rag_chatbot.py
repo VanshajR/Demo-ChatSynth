446c012d154b0ad1a2aadb9c3d1780b87f1b8364
Vanshaj Raghuvanshi <vanshajraghuvanshi@gmail.com>
2025-03-22 13:59:55 +0530

    Update rag_chatbot.py

--- rag_chatbot.py
+++ rag_chatbot.py
@@ -43,7 +43,7 @@ def chat_assistant():
     # Sidebar settings
     with st.sidebar:
         st.header("⚙️ Settings")
-        model_name = st.selectbox("🤖 Choose LLM Model:", ["Llama3-70b-8192", "mixtral-8x7b-32768"])
+        model_name = st.selectbox("🤖 Choose LLM Model:", ["Llama3-70b-8192", "gemma2-9b-it", "mixtral-8x7b-32768"])
         st.write("Powered By ChatSynth")
         st.write("https://chatsynth.streamlit.app")
 
